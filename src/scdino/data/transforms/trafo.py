94f24fe016fef7e73b28b1c0567b93f1c605fbaa
JonasMeirer <jonasmeirer@gmail.com>
2026-04-28 16:26:49 +0200

    datamodule updates

--- src/scdino/data/transforms/trafo.py
+++ src/scdino/data/transforms/trafo.py
@@ -11,7 +11,7 @@ class Trafo:
             # no transform
             self.transform = lambda x: x
         else:
-            if model.name in ["dinov2", "dino"]:
+            if model.name in ["dinov2", "dino", "dinov2_StrucPerc"]:
                 self.transform = DINOTransform(cfg)
             else:
                 raise ValueError(
